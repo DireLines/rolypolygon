bda017c58e163350cb4a05c370267ed6571ebc9b
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2021-11-24 14:48:07 -0500

    project position onto mesh

--- Roly-Polygon/Assets/Scripts/TravelOverMesh.cs
+++ Roly-Polygon/Assets/Scripts/TravelOverMesh.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-public class GetCurrentPolygon : MonoBehaviour {
+public class TravelOverMesh : MonoBehaviour {
     PolygonInfo polygon;
     public LayerMask layerMask;
     MeshNavigation meshNavigation;
@@ -27,9 +27,27 @@ public class GetCurrentPolygon : MonoBehaviour {
         if (Physics.Raycast(transform.position, rayDir, out RaycastHit hit, 1000f, layerMask)) {
             polygon = new PolygonInfo(transform.parent.GetComponent<MeshFilter>().mesh, hit.triangleIndex, transform.parent);
         }
-        //Vector3 targetPos = Vector3.ProjectOnPlane(transform.position - polygon.points[0], polygon.normal) + polygon.points[0] + polygon.normal.normalized * 0.001f;
-        //update velocity to be within the plane of the face
-        //GetComponent<Rigidbody>().velocity = 50 * Time.deltaTime * (targetPos - transform.position);
+        foreach (List<Vector3> l in meshNavigation.getNeighboringFaces(polygon.triangleIndex)) {
+            Vector3 neighboringNormal = PolygonMath.getNormalFromPoints(l);
+            List<Vector3> commonPoints = PolygonMath.commonPoints(polygon.points, l);
+            List<Vector3> unfoldedPoints = new List<Vector3>();
+            foreach (Vector3 p in l) {
+                unfoldedPoints.Add(PolygonMath.rotateAround(
+                        p,
+                        commonPoints[0],
+                        Quaternion.FromToRotation(
+                            neighboringNormal,
+                            polygon.normal)));
+            }
+            Vector3 faceCenter = (polygon.points[0] + polygon.points[1] + polygon.points[2]) / 3f;
+            Func<Vector3, Vector3> meshToPlane = (point) => Vector3.ProjectOnPlane(Quaternion.FromToRotation(polygon.normal, Vector3.up) * (point - faceCenter), Vector3.up);
+            Func<Vector3, Vector3> planeToMesh = (point) => Quaternion.FromToRotation(Vector3.up, polygon.normal) * point + faceCenter;
+            List<Vector3> planarPoints = new List<Vector3>();
+            foreach (Vector3 p in unfoldedPoints) {
+                planarPoints.Add(meshToPlane(p));
+            }
+            transform.position = planeToMesh(meshToPlane(transform.position));
+        }
     }
     void DrawTriangle(List<Vector3> corners) {
         foreach (Vector3 p in corners) {
@@ -59,14 +77,15 @@ public class GetCurrentPolygon : MonoBehaviour {
             }
             Gizmos.color = Color.blue;
             DrawTriangle(unfoldedPoints);
-            Func<Vector3, Vector3> rotateOntoPlane = (point) => Vector3.ProjectOnPlane(Quaternion.FromToRotation(polygon.normal, Vector3.up) * point, Vector3.up);
+            Vector3 faceCenter = (polygon.points[0] + polygon.points[1] + polygon.points[2]) / 3f;
+            Func<Vector3, Vector3> meshToPlane = (point) => Vector3.ProjectOnPlane(Quaternion.FromToRotation(polygon.normal, Vector3.up) * (point - faceCenter), Vector3.up);
+            Func<Vector3, Vector3> planeToMesh = (point) => Quaternion.FromToRotation(Vector3.up, polygon.normal) * point + faceCenter;
             List<Vector3> planarPoints = new List<Vector3>();
             foreach (Vector3 p in unfoldedPoints) {
-                planarPoints.Add(rotateOntoPlane(p) - rotateOntoPlane(transform.position));
+                planarPoints.Add(meshToPlane(p));
             }
             Gizmos.color = Color.green;
             DrawTriangle(planarPoints);
-            Gizmos.DrawSphere(rotateOntoPlane(transform.position) - rotateOntoPlane(transform.position), 0.04f);
         }
     }
 }
