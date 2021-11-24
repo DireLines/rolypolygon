6e606e4404b709c81ce5ef2c5c05d164333653b6
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2021-11-23 23:44:43 -0500

    project onto plane

--- Roly-Polygon/Assets/Scripts/GetCurrentPolygon.cs
+++ Roly-Polygon/Assets/Scripts/GetCurrentPolygon.cs
@@ -31,14 +31,22 @@ public class GetCurrentPolygon : MonoBehaviour {
         //update velocity to be within the plane of the face
         //GetComponent<Rigidbody>().velocity = 50 * Time.deltaTime * (targetPos - transform.position);
     }
+    void DrawTriangle(List<Vector3> corners) {
+        foreach (Vector3 p in corners) {
+            Gizmos.DrawSphere(p, 0.01f);
+        }
+        for (int i = 0; i < 3; i++) {
+            Gizmos.DrawLine(corners[i], corners[(i + 1) % 3]);
+        }
+    }
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         foreach (Vector3 p in polygon.points) {
             Gizmos.DrawSphere(p, 0.015f);
         }
         Gizmos.DrawLine(transform.position, transform.position + polygon.normal);
-        Gizmos.color = Color.blue;
         foreach (List<Vector3> l in meshNavigation.getNeighboringFaces(polygon.triangleIndex)) {
+            Vector3 neighboringNormal = PolygonMath.getNormalFromPoints(l);
             List<Vector3> commonPoints = PolygonMath.commonPoints(polygon.points, l);
             List<Vector3> unfoldedPoints = new List<Vector3>();
             foreach (Vector3 p in l) {
@@ -46,15 +54,19 @@ public class GetCurrentPolygon : MonoBehaviour {
                         p,
                         commonPoints[0],
                         Quaternion.FromToRotation(
-                            PolygonMath.getNormalFromPoints(l),
+                            neighboringNormal,
                             polygon.normal)));
             }
+            Gizmos.color = Color.blue;
+            DrawTriangle(unfoldedPoints);
+            Func<Vector3, Vector3> rotateOntoPlane = (point) => Vector3.ProjectOnPlane(Quaternion.FromToRotation(polygon.normal, Vector3.up) * point, Vector3.up);
+            List<Vector3> planarPoints = new List<Vector3>();
             foreach (Vector3 p in unfoldedPoints) {
-                Gizmos.DrawSphere(p, 0.01f);
+                planarPoints.Add(rotateOntoPlane(p) - rotateOntoPlane(transform.position));
             }
-            for (int i = 0; i < 3; i++) {
-                Gizmos.DrawLine(unfoldedPoints[i], unfoldedPoints[(i + 1) % 3]);
-            }
+            Gizmos.color = Color.green;
+            DrawTriangle(planarPoints);
+            Gizmos.DrawSphere(rotateOntoPlane(transform.position) - rotateOntoPlane(transform.position), 0.04f);
         }
     }
 }
